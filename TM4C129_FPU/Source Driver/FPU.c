8feab05939bf4977924163e1cd0216a5875182d7
Eduardo Alvarez <avalanch_m@hotmail.com>
2020-03-14 21:22:49 -0600

    [TM4C129_FPU] Update FPU.c, FPU__vInit()

--- TM4C129_FPU/Source Driver/FPU.c
+++ TM4C129_FPU/Source Driver/FPU.c
@@ -161,9 +161,9 @@ void FPU__vInit(void)
     FPU__vSetAccessType(FPU_enFULL_ACCESS);
     FPU__vSetLazyPreservation(FPU_enLAZY_PRESERVATION_EN);
     FPU__vSetAutoPreservation(FPU_enAUTO_PRESERVATION_EN);
-    FPU__vSetHalfPrecision(FPU_enHALF_PRECISION_IEEE);
-    FPU__vSetNAN(FPU_enNAN_PROPAGATE);
-    FPU__vSetRounding(FPU_enROUNDING_NEAREST);
+    //FPU__vSetHalfPrecision(FPU_enHALF_PRECISION_IEEE);
+    //FPU__vSetNAN(FPU_enNAN_PROPAGATE);
+    //FPU__vSetRounding(FPU_enROUNDING_NEAREST);
 }
 
 
